0d2dc46461f9c184fb3b74c21fbd78774d061d7b
Sampath <sam1120@outlook.com>
2022-05-30 02:23:22 +0530

    Custome themes

--- app.py
+++ app.py
@@ -1,157 +1,37 @@
 import streamlit as st
 import streamlit.components.v1 as stc
+import pandas as pd
 
 
 def main():
-    st.title("Components")
-    stc.html('<p style="color:red">Streamlit is Awesome</p>')
-    st.markdown(
-        '<p style="color:blue">Streamlit is Awesome</p>', unsafe_allow_html=True
+    st.title("Welcome to Streamlit!")
+
+    st.write("Our first DataFrame")
+
+    st.write(
+    pd.DataFrame({
+        'A': [1, 2, 3, 4],
+        'B': [5, 6, 7, 8]
+        })
     )
 
-    stc.html(
-        """
-<style>
-* {box-sizing: border-box}
-body {font-family: Verdana, sans-serif; margin:0}
+    checkbox_one = st.checkbox("Yes")
+    checkbox_two = st.checkbox("No")
 
-/* Slideshow container */
-.slideshow-container {
-  position: relative;
-  background: #f1f1f1f1;
-}
+    if checkbox_one:
+        value = "Yes"
+    elif checkbox_two:
+        value = "No"
+    else:
+        value = "No value selected"
 
-/* Slides */
-.mySlides {
-  display: none;
-  padding: 10px;
-  text-align: center;
-}
+    st.write(f"You selected: {value}")
+    start_x = st.sidebar.slider("Start X", value= 24, key='sx')
+    start_y = st.sidebar.slider("Start Y", value= 332, key='sy')
+    finish_x = st.sidebar.slider("Finish X", value= 309, key='fx')
+    finish_y = st.sidebar.slider("Finish Y", value= 330, key='fy')
 
-/* Next & previous buttons */
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  margin-top: -30px;
-  padding: 16px;
-  color: #888;
-  font-weight: bold;
-  font-size: 20px;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-}
-
-/* Position the "next button" to the right */
-.next {
-  position: absolute;
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
-
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover, .next:hover {
-  background-color: rgba(0,0,0,0.8);
-  color: white;
-}
-
-/* The dot/bullet/indicator container */
-.dot-container {
-    text-align: center;
-    padding: 20px;
-    background: #ddd;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  cursor: pointer;
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-/* Add a background color to the active dot/circle */
-.active, .dot:hover {
-  background-color: #717171;
-}
-
-/* Add an italic font style to all quotes */
-q {font-style: italic;}
-
-/* Add a blue color to the author */
-.author {color: cornflowerblue;}
-</style>
-</head>
-<body>
-
-<div class="slideshow-container">
-
-<div class="mySlides">
-  <q>I love you the more in that I believe you had liked me for my own sake and for nothing else</q>
-  <p class="author">- John Keats</p>
-</div>
-
-<div class="mySlides">
-  <q>But man is not made for defeat. A man can be destroyed but not defeated.</q>
-  <p class="author">- Ernest Hemingway</p>
-</div>
-
-<div class="mySlides">
-  <q>I have not failed. I've just found 10,000 ways that won't work.</q>
-  <p class="author">- Thomas A. Edison</p>
-</div>
-
-<a class="prev" onclick="plusSlides(-1)">❮</a>
-<a class="next" onclick="plusSlides(1)">❯</a>
-
-</div>
-
-<div class="dot-container">
-  <span class="dot" onclick="currentSlide(1)"></span> 
-  <span class="dot" onclick="currentSlide(2)"></span> 
-  <span class="dot" onclick="currentSlide(3)"></span> 
-</div>
-
-<script>
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-</script>
-
-</body>
 
     
-    """
-    )
-    st.write("# Iframe")
-    stc.iframe("https://fr.wikipedia.org/wiki/Main_Page")
 if __name__ == "__main__":
     main()
