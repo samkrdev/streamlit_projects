217472d12372c33004eaa91b9248952dc56d9729
Sampath <sam1120@outlook.com>
2022-06-05 15:22:54 +0530

    TODO App

--- app.py
+++ app.py
@@ -1,77 +1,101 @@
-import time  # to simulate a real time data, time loop
-import numpy as np  # np mean, np random
-import pandas as pd  # read csv, df manipulation
-import plotly.express as px  # interactive charts
-import streamlit as st  # 🎈 data web app development
+import streamlit as st
+import pandas as pd
+from db_fxns import *
+import plotly.express as px
 
-# read csv from a github repo
-dataset_url = "https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv"
-# read csv from a URL
-@st.experimental_memo
-def get_data() -> pd.DataFrame:
-    return pd.read_csv(dataset_url)
+def main():
+    st.set_page_config(layout="wide")
+    st.title("TODO App")
     
-st.set_page_config(
-    page_title="Real-Time Data Science Dashboard",
-    page_icon="✅",
-    layout="wide",
-)
+    menu = ["Create", "Read", "Update", "Delete", "About"]
 
-df = get_data()
+    choice = st.sidebar.selectbox("Menu", menu)
+    create_table()
+    if choice == "Create":
+        st.subheader("Add Items")
+
+        # layout
+        col1, col2 = st.columns(2)
+        with col1:
+            task = st.text_area("Task To Do")
+        with col2:
+            task_status = st.selectbox("Status", ["ToDo", "Doing", "Done"])
+            task_due_date = st.date_input("Due Date")
+
+        if st.button("Add Task"):
+            add_data(task,task_status,task_due_date)
+            st.success(f"Successfully Added Data:{task}")
+
+    elif choice == "Read":
+        st.subheader("View Items")
+        result = view_all_data()
+        df = pd.DataFrame(result,columns=['Task','Status','Due Date'])
+        with st.expander('View All Data'):
+            st.dataframe(df)
+
+        with st.expander('Task Status'):
+            task_df = df['Status'].value_counts().to_frame().reset_index()
+            st.dataframe(task_df)
+            pl = px.pie(task_df,names='index',values='Status')
+            st.plotly_chart(pl)
+
+    elif choice == "Update":
+        st.subheader("Edit/Update Items")
+        result = view_all_data()
+        df = pd.DataFrame(result,columns=['Task','Status','Due Date'])
+        with st.expander("Current Data"):
+            st.dataframe(df)
+        list_of_tasks = [i[0] for i in view_all_task_names()]
+        selected_task = st.selectbox("Task",list_of_tasks)
+        task_result = get_task(selected_task)
+        st.write(task_result)
+        if task_result:
+            task = task_result[0][0]
+            task_status = task_result[0][1]
+            task_due_date = task_result[0][2]
+
+            col1,col2 = st.columns(2)
+
+            with col1:
+                new_task = st.text_area("Task To Do",task)
+
+            with col2:
+                values = ["ToDo","Doing","Done"]
+                default_ix = values.index(task_status)
+                new_task_status = st.selectbox("Status",values,index=default_ix)
+                new_task_due_date = st.date_input(task_due_date)
+
+            if st.button("Update Task"):
+                edit_task_data(new_task,new_task_status,new_task_due_date,task,task_status,task_due_date)
+                st.success("Updated ::{} ::To {}".format(task,new_task))
+
+            with st.expander("View Updated Data"):
+                result = view_all_data()
+            # st.write(result)
+            clean_df = pd.DataFrame(result,columns=["Task","Status","Date"])
+            st.dataframe(clean_df)
+    elif choice == "Delete":
+        st.subheader("Delete Item")
+        with st.expander("View Data"):
+            result = view_all_data()
+            # st.write(result)
+            clean_df = pd.DataFrame(result,columns=["Task","Status","Date"])
+            st.dataframe(clean_df)
+
+        unique_list = [i[0] for i in view_all_task_names()]
+        delete_by_task_name =  st.selectbox("Select Task",unique_list)
+        if st.button("Delete"):
+            delete_data(delete_by_task_name)
+            st.warning("Deleted: '{}'".format(delete_by_task_name))
+
+        with st.expander("Updated Data"):
+            result = view_all_data()
+            # st.write(result)
+            clean_df = pd.DataFrame(result,columns=["Task","Status","Date"])
+            st.dataframe(clean_df)
+    else:
+        st.subheader("About")
 
 
-# dashboard title
-st.title("Real-Time / Live Data Science Dashboard")
-# top-level filters
-job_filter = st.selectbox("Select the Job", pd.unique(df["job"]))
-# creating a single-element container
-placeholder = st.empty()
-# dataframe filter
-df = df[df["job"] == job_filter]
-# near real-time / live feed simulation
-
-for seconds in range(200):
-    df["age_new"] = df["age"] * np.random.choice(range(1, 5))
-    df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
-    # creating KPIs
-    avg_age = np.mean(df["age_new"])
-    count_married = int(
-        df[(df["marital"] == "married")]["marital"].count()
-        + np.random.choice(range(1, 30))
-    )
-    balance = np.mean(df["balance_new"])
-    with placeholder.container():
-        # create three columns
-        kpi1, kpi2, kpi3 = st.columns(3)
-        # fill in those three columns with respective metrics or KPIs
-        kpi1.metric(
-            label="Age ⏳",
-            value=round(avg_age),
-            delta=round(avg_age) - 10,
-        )
-
-        kpi2.metric(
-            label="Married Count 💍",
-            value=int(count_married),
-            delta=-10 + count_married,
-        )
-
-        kpi3.metric(
-            label="A/C Balance ＄",
-            value=f"$ {round(balance,2)} ",
-            delta=-round(balance / count_married) * 100,
-        )
-        # create two columns for charts
-        fig_col1, fig_col2 = st.columns(2)
-        with fig_col1:
-            st.markdown("### First Chart")
-            fig = px.density_heatmap(data_frame=df, y="age_new", x="marital")
-            st.write(fig)
-
-        with fig_col2:
-            st.markdown("### Second Chart")
-            fig2 = px.histogram(data_frame=df, x="age_new")
-            st.write(fig2)
-        st.markdown("### Detailed Data View")
-        st.dataframe(df)
-        time.sleep(1)
+if __name__ == "__main__":
+    main()
